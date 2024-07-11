b47d08d117caeb0dd9dff75c5ad0513a466e8946
Sameer Hasan <simplysamy@gmail.com>
2024-07-11 16:01:12 +0530

    Add all the files

---
+++ resource.tf
@@ -0,0 +1,46 @@
+resource "aws_default_vpc" "default" {}
+
+resource "aws_instance" "new_instance" {
+  count                  = var.instance_count
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+}
+
+
+
+resource "aws_security_group" "instance_sg" {
+  name_prefix = "instance_sg"
+  description = "Allow inbound traffic on port 22, 80 & 8080"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
